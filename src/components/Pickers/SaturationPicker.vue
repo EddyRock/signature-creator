b45e4a20e12ea940377e2fcfb478eba9eebe0221
Eduard Rudnev <eduardrudnev@MacBook-Pro-Eduard.local>
2022-11-04 03:56:56 +0100

    first commit

---
+++ src/components/Pickers/SaturationPicker.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SaturationPicker"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SelectColorPicker.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SelectColorPicker"
+}
+</script>
+
+<style scoped>
+
+</style>
